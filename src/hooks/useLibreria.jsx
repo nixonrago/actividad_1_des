9706f1fe370b854ed0f47348e4f49586a65c9e97
nixon <nixonmauricio.ramon000@comunidadunir.net>
2024-12-29 17:59:03 -0600

    Aactividad 1

--- src/hooks/useLibreria.jsx
+++ src/hooks/useLibreria.jsx
@@ -5,12 +5,9 @@ export const useLibreria = () => {
 
 
     const [Libros, setLibros] = useState([]);
-    /**
-     * Se hace uso de useEffect para definir un efecto de montaje que traerá la información de ingredientes
-     * del back-end en el primer renderizado.
-     */
+
     useEffect(() => {
-        //fetch(process.env.REACT_APP_GW_URL).then((res) => res.json()).then((res) => setRestaurants(res));
+
         const getImagen = (index) => require(`../imagenes/imagen${index}.PNG`);
 
         setTimeout(() => {

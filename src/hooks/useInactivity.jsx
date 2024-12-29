0034e58b940a61ebcba933fef0abd5c3c98e64e1
nixon <nixonmauricio.ramon000@comunidadunir.net>
2024-12-29 17:39:25 -0600

    Aactividad 1

--- src/hooks/useInactivity.jsx
+++ src/hooks/useInactivity.jsx
@@ -8,29 +8,27 @@ const useInactivityRedirect = (path, delay = 5000) => {
     const timeoutRef = useRef(null);
 
     const resetTimer = () => {
-        // Limpia el temporizador anterior
+
         if (timeoutRef.current) {
             clearTimeout(timeoutRef.current);
         }
 
-        // Establece un nuevo temporizador de inactividad
         timeoutRef.current = setTimeout(() => {
 
-            navigate(path); // Redirige al usuario a la página principal
+            navigate(path);
         }, delay);
     };
 
     useEffect(() => {
-        // Agregar event listeners para detectar actividad
+
         const events = ['mousemove', 'keydown', 'click', 'scroll'];
         events.forEach(event =>
             window.addEventListener(event, resetTimer)
         );
 
-        // Iniciar el temporizador de inactividad cuando el componente se monta
         resetTimer();
 
-        // Limpiar los event listeners al desmontar el componente
+
         return () => {
             events.forEach(event =>
                 window.removeEventListener(event, resetTimer)
